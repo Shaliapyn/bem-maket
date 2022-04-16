2d9982c3fa319616031098cfeb2663cadc5b7939
Ihor Shalapa <igorshalapa@gmail.com>
2022-04-16 14:07:25 +0300

    complete contactUs comp

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <div className='ContactUs'>
+      <form className='ContactUs_form' action="">
+        <h2 className='ContactUs__title'>Contact us</h2>
+        <div className="ContactUs__inputs">
+          <input className='ContactUs__inp--name ContactUs-inp' placeholder='Name' type="text" />
+          <input className='ContactUs__inp--numb ContactUs-inp' placeholder='Phone' type="number" />
+          <input className='ContactUs__inp--email ContactUs-inp' placeholder='Email' type="email" />
+          <textarea className='ContactUs__message' placeholder='Message' name="" id="" cols="30" rows="10"></textarea>
+        </div>
+        <button className='ContactUs-btn'>Send</button>
+        <p className='ContactUs_footer-text'>Privacy policy</p>
+      </form>
+    </div>
   )
 }
 
