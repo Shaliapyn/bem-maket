2bc1f15da06d5f2e05dbb82aa49830dcf867150d
Ihor Shalapa <igorshalapa@gmail.com>
2022-04-19 10:47:40 +0300

    add correct quotes

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react"
 
 const ContactUs = () => {
   return (
-    <div className='ContactUs'>
-      <form className='ContactUs_form' action="">
-        <h2 className='ContactUs__title'>Contact us</h2>
+    <div className="ContactUs">
+      <form className="ContactUs_form" action="">
+        <h2 className="ContactUs__title">Contact us</h2>
         <div className="ContactUs__inputs">
-          <input className='ContactUs__inp--name ContactUs-inp' placeholder='Name' type="text" />
-          <input className='ContactUs__inp--numb ContactUs-inp' placeholder='Phone' type="number" />
-          <input className='ContactUs__inp--email ContactUs-inp' placeholder='Email' type="email" />
-          <textarea className='ContactUs__message' placeholder='Message' name="" id="" cols="30" rows="10"></textarea>
+          <input className="ContactUs__inp--name ContactUs-inp" placeholder="Name" type="text" />
+          <input className="ContactUs__inp--numb ContactUs-inp" placeholder="Phone" type="number" />
+          <input className="ContactUs__inp--email ContactUs-inp" placeholder="Email" type="email" />
+          <textarea className="ContactUs__message" placeholder="Message" name="" id="" cols="30" rows="10"></textarea>
         </div>
-        <button className='ContactUs-btn'>Send</button>
-        <p className='ContactUs_footer-text'>Privacy policy</p>
+        <button className="ContactUs-btn">Send</button>
+        <p className="ContactUs_footer-text">Privacy policy</p>
       </form>
     </div>
   )
--- src/components/HeaderBody/HeaderBody.js
+++ src/components/HeaderBody/HeaderBody.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react"
 
 const HeaderBody = () => {
   return (
-    <div className='main'>
+    <div className="main">
       <div className="absolute-img--figure"></div>
       <div className="absolute-img--woman"></div>
 
       <div className="main__title-container">
         <div className="main__title-block">
           <div className="main__title-block--little">
-            <hr className='main__line--blue'/>
-            <h3 className='main__title--little'>WTG SPAIN</h3>
+            <hr className="main__line--blue"/>
+            <h3 className="main__title--little">WTG SPAIN</h3>
           </div>
           <div className="main__title-block--big">
-            <h1 className='main__title--big'>Do you need <span className='blue-text'>some help</span>?</h1>
-            <p className='main__title--big-text'>Contact us right now and our team will do everything to help you.</p>
+            <h1 className="main__title--big">Do you need <span className="blue-text">some help</span>?</h1>
+            <p className="main__title--big-text">Contact us right now and our team will do everything to help you.</p>
           </div>
-          <button className='main__title-button'>Contact us</button>
+          <button className="main__title-button">Contact us</button>
         </div>
       </div>
     </div>
